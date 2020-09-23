02b34eef1b23c72094acc548bf885aa2530abc73
Iluhander <iisergeev_2@edu.hse.ru>
2020-09-24 01:05:13 +0300

    .

--- module_1/HW2/Task1/Task1/Program.cs
+++ module_1/HW2/Task1/Task1/Program.cs
@@ -1,12 +1,60 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Task1
 {
     class Program
     {
-        static void Main(string[] args)
+        /*
+           Метод для получения информации от пользователя
+           * Если пользователь вводит пустую строку, возвращает 2
+           * Если введённая пользователем информация некорректна, возвращает 1
+           * Иначе возвращает 0
+        */
+
+        static sbyte Get_User_Data (ref double x)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите x\nДля завершения работы программы введите пустую строку");
+
+            string x_string = Console.ReadLine();
+
+            //Если пользователь ввёл пустую строку, возвращаем 2
+            if (x_string == "")
+                return 2;
+
+            bool x_parsed = double.TryParse(x_string, out x);
+
+            //Если введённые данные некорректны, возвращаем 1
+            if (!x_parsed)
+                return 1;
+            //Иначе 0 (нет каких-либо ошибок или исключений)
+            else
+                return 0;
+        }
+
+        static void Main(string[] args)
+        {   
+            double x = 0;
+
+            //Цикл для повторения решения 
+            int code = Get_User_Data(ref x);
+            while (code < 2)
+            {
+                // Если введённые данные корректны
+                if (code == 0)
+                {
+                    double result = x * ( x * ( x * (12 * x + 9) - 3 ) + 2) - 4;
+                    Console.WriteLine(result);
+                }
+                //Иначе
+                else
+                    Console.WriteLine("Введённые данные некорректны\nПопробуйте еще раз");
+
+                Console.WriteLine();
+
+                //Еще раз получаем данные от пользователя
+                code = Get_User_Data(ref x);
+            }
         }
     }
 }
