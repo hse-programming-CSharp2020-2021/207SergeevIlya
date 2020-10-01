8372525b4779e0725097c021ca22ca6679d9f1f3
Iluhander <iisergeev_2@edu.hse.ru>
2020-10-01 19:30:12 +0300

    .

---
+++ module_1/SEM_5/Task2/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+            
+            Random rand = new Random();
+            for (int i = 0; i < n; ++i)
+                arr[i] = rand.Next(-10, 11);
+
+
+
+            Console.WriteLine("Было : ");
+            for (int i = 0; i < n; ++i)
+                Console.WriteLine(arr[i]);
+
+
+            int m = 0;
+            for (int i = 0, j = 0; i < n; ++i)
+                if (Math.Abs(arr[i]) % 2 == 0)
+                {
+                    arr[j] = arr[i];
+                    m++;
+                    j++;
+                }
+
+            if (m > 0)
+                Array.Resize(ref arr, n - m);
+
+            Console.WriteLine("Стало : ");
+            for (int i = 0; i < n - m; ++i)
+                Console.WriteLine(arr[i]);
+        }
+    }
+}
