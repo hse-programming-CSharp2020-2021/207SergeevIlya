f2a1bab8b43fd17e0414d6e9eb63276bd658fb13
Iluhander <iisergeev_2@edu.hse.ru>
2020-10-01 19:00:06 +0300

    .

---
+++ module_1/SEM_5/Task1/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Task1
+{
+    class Program
+    {
+        static char [] num_to_char_arr (int num)
+        {
+            int n; 
+            if (num != 0)
+                n = (int)Math.Log10(((double)num)) + 1;
+            else
+                n = 1;
+
+
+            char[] arr = new char[n];
+
+            if (num == 0)
+                arr[0] = '0';
+
+            int i = n - 1;
+            while (num > 0)
+            {
+                arr[i] = (char)(num % 10 + '0');
+
+                num /= 10;
+                i--;
+            }
+
+            return arr;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            char[] arr = num_to_char_arr(n);
+
+            for (int i = 0; i < arr.Length; ++i)
+                Console.WriteLine(arr[i]);
+        }
+    }
+}
