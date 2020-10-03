300c85ac48824ccd95143fa1f8a96369da6b62bc
Iluhander <iisergeev_2@edu.hse.ru>
2020-10-03 14:30:53 +0300

    .

---
+++ module_1/SEM_6/Task_arr/Task_arr/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task_arr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arr = new int[n];
+ 
+            for (int i = 0; i < n; ++i)
+                arr[i] = int.Parse(Console.ReadLine());
+
+            int m = 0;
+            for (int i = 0; i < n; ++i)
+                if (arr[i] >= 0)
+                    m++;
+
+            int[] res_arr = new int[n];
+            for (int i = 0, j = 0, k = m; i < n; ++i)
+                if (arr[i] >= 0)
+                {
+                    res_arr[j] = arr[i];
+                    j++;
+                }
+                else
+	            {
+                    res_arr[k] = arr[i];
+                    k++;
+	            }
+
+            for (int i = 0; i < n; ++i)
+                Console.WriteLine(res_arr[i]);
+                    
+        }
+    }
+}
