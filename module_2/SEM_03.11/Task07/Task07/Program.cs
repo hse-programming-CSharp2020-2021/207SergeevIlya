74c206ab0b76a593772e0a9e007644af10032ada
Iluhander <iisergeev_2@edu.hse.ru>
2020-11-03 16:04:10 +0300

    .

---
+++ module_2/SEM_03.11/Task07/Task07/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using static System.Console;
+
+namespace Task07
+{
+    public class SIN
+    {
+        private double XMin, XMax;
+
+        public SIN(double min, double max)
+        {
+            XMin = min;
+            XMax = max;
+        }
+
+        public double this[double Arg]
+        {
+            get
+            {
+                if (Arg < XMin || Arg > XMax)
+                    return 0;
+                return Math.Sin(Arg);
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SIN UserSin = new SIN(0, Math.PI);
+
+            double Arg = double.Parse(ReadLine());
+            WriteLine("Значение функции равно: " + UserSin[Arg / 6 * Math.PI]);
+        }
+    }
+}
