651dd88a954e9e7cf5474ed20607bb6394190c01
Iluhander <iisergeev_2@edu.hse.ru>
2021-01-17 02:31:34 +0300

    .

---
+++ module_3/HW_19.01/Task_01/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Task_01
+{
+    /// <summary>
+    /// Класс, в котором реализуются методы для перевода температуры в другие единицы измерения.
+    /// </summary>
+    internal class TemperatureConverterImp
+    {
+        public double CelcToFar(double tCelc)
+        {
+            return (tCelc * 9.0 / 5 + 32.0);
+        }
+
+        public double FarToCelc(double tFar)
+        {
+            return ((tFar - 32) * 5.0 / 9);
+        }
+
+        public double CelcToRan(double tCelc)
+        {
+            return (tCelc * 9.0 / 5 + 491.67);
+        }
+
+        public double RanToCelc(double tRan)
+        {
+            return ((tRan - 491.67) * 5.0 / 9 );
+        }
+
+        public double CelcToReo(double tCelc)
+        {
+            return (tCelc * 1.25);
+        }
+
+        public double ReoToCelc(double tReo)
+        {
+            return (tReo / 1.25);
+        }
+    }
+
+    class Program
+    {
+        delegate double delegateConvertTemperature(double p);
+
+        /// <summary>
+        /// Метод для проверки корректности ввода.
+        /// Записывает в переменную res -1, если ввод некорректен.
+        /// Записывает в res -2, если пользователь решил отменить ввод.
+        /// Иначе записывает номер делегата, который потребуется вызвать.
+        /// </summary>
+        /// <returns>
+        /// Если ввод корректен, true.
+        /// Иначе, false.
+        /// </returns>
+        static bool Validate (string inp, out int res)
+        {
+            inp = inp.Trim(' ');
+
+            if (inp == "-")
+                res = -2;
+            else
+            {
+                string[] inpSplitted = inp.Split(' ');
+
+                if (inpSplitted.Length != 2)
+                    res = -1;
+                else if (inpSplitted[0] == "tC" && inpSplitted[1] == "tF")
+                    res = 0;
+                else if (inpSplitted[0] == "tF" && inpSplitted[1] == "tC")
+                    res = 1;
+                else if (inpSplitted[0] == "tC" && inpSplitted[1] == "tRa")
+                    res = 2;
+                else if (inpSplitted[0] == "tRa" && inpSplitted[1] == "tC")
+                    res = 3;
+                else if (inpSplitted[0] == "tC" && inpSplitted[1] == "tRe")
+                    res = 4;
+                else if (inpSplitted[0] == "tRe" && inpSplitted[1] == "tC")
+                    res = 5;
+                else
+                    res = -1;
+            }
+
+            return (res != -1);
+        }
+
+        static void Main(string[] args)
+        {
+            TemperatureConverterImp methodsClass = new TemperatureConverterImp();
+
+            // Собственно, массив делигатов.
+            delegateConvertTemperature[] delArray = new delegateConvertTemperature[6];
+            delArray[0] = methodsClass.CelcToFar;
+            delArray[1] = methodsClass.FarToCelc;
+            delArray[2] = methodsClass.CelcToRan;
+            delArray[3] = methodsClass.RanToCelc;
+            delArray[4] = methodsClass.CelcToReo;
+            delArray[5] = methodsClass.ReoToCelc;
+
+            // Выводим таблицу и необходимую информацию.
+            // (Решил несколько расширить функционал и сделать ввод поудобнее).
+            Console.WriteLine("                |  Из цельсия (tC)          | В цельсия (tC)");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("Фаренгейты (tF) |  tF = 9 / 5 * tC + 32     | tC = 5 / 9 * (tF - 32)");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("Ранкины (tRa)   | tC * 9.0 / 5 + 491.67     | (tRa - 491.67) * 5.0 / 9 ");
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("Реомюры (tRe)   | tC * 5 / 4                | tRe * 4 / 5 ");
+            Console.WriteLine("----------------------------------------------------------------------\n");
+
+            do
+            {
+                Console.WriteLine("Введите через пробел наименование величины, из которой хотите перевести, и " +
+                    "величину, в которую хотите перевести \n(переводить можно либо из градусов цельсия, либо в них)");
+                Console.WriteLine("Прим.: tC tRe");
+                Console.WriteLine("Также вы можете ввести '-', чтобы отменить ввод.\n");
+
+                // Получаем от пользователя информацию о том, из какой величины он хочет перевести и в какую.
+                int inp;
+                while (!Validate(Console.ReadLine(), out inp))
+                    Console.WriteLine("Ввод некорректен. Попробуйте ещё раз или введите '-', чтобы отменить ввод и завершить работу.");
+
+                // Если пользователь решил отменить ввод и завершить выполнение программы.
+                if (inp == -2)
+                    return;
+
+                string[] delNames = new string[6] { "градусах цельсия", "фаренгейтах", "градусах цельсия", "ранкинах", "градусах цельсия", "реомюрах" };
+
+                // Получаем от пользователя температуру в градусах цельсия.
+                Console.WriteLine($"Введите температуру в {delNames[inp]}");
+
+                double inpT;
+                while (!double.TryParse(Console.ReadLine(), out inpT))
+                    Console.WriteLine("Введено некорректное значение. Попробуйте ещё раз.");
+
+                Console.WriteLine($"Переведённое значение температуры: {delArray[inp](inpT)}\n");
+            }
+            while (true);
+        }
+    }
+}
---
+++ module_3/HW_19.01/Task_02/Program.cs
@@ -0,0 +1,325 @@
+﻿using System;
+
+namespace Task_02
+{
+    /// <summary>
+    /// Класс для представления робота.
+    /// </summary>
+    class Robot
+    {
+        public int x, 
+                   y;
+
+        public int maxX,
+                   maxY;
+
+        /// <summary>
+        /// Ссылка на поле, по которому перемещается робот.
+        /// </summary>
+        public char[,] area;
+
+        /// <summary>
+        /// Выходил ли робот за границы поля.
+        /// </summary>
+        public bool leftArea = false;
+
+        /// <summary>
+        /// Метод, проверяющий, не вышел ли робот за границы поля.
+        /// </summary>
+        private void Check ()
+        {
+            leftArea = leftArea || (x < 0) || (y < 0) || (x >= maxX) || (y >= maxY);
+        }
+
+        /// <summary>
+        /// Метод, передвигающий робота направо.
+        /// </summary>
+        public void Right()
+        {
+            if (leftArea)
+                return;
+
+            x++;
+            Check();
+
+            if (!leftArea)
+            {
+                area[y + 1, x] = '+';
+                area[y + 1, x + 1] = '*';
+            }
+                
+        }
+
+        /// <summary>
+        /// Метод, передвигающий робота налево.
+        /// </summary>
+        public void Left()
+        {
+            if (leftArea)
+                return;
+
+            x--;
+            Check();
+
+            if (!leftArea)
+            {
+                area[y + 1, x + 2] = '+';
+                area[y + 1, x + 1] = '*';
+            }
+                
+        }
+
+        /// <summary>
+        /// Метод, передвигающий робота вперед.
+        /// </summary>
+        public void Forward()
+        {
+            if (leftArea)
+                return;
+
+            y++;
+            Check();
+
+            if (!leftArea)
+            {
+                area[y, x + 1] = '+';
+                area[y + 1, x + 1] = '*';
+            }
+                
+        }
+
+        /// <summary>
+        /// Метод, передвигающий робота назад.
+        /// </summary>
+        public void Backward()
+        {
+            if (leftArea)
+                return;
+
+            y--;
+            Check();
+
+            if (!leftArea)
+            {
+                area[y + 2, x + 1] = '+';
+                area[y + 1, x + 1] = '*';
+            }
+                
+        }
+
+        /// <summary>
+        /// Метод, позволяющий роботу отдохнуть слегка.
+        /// </summary>
+        public void Stay ()
+        {
+
+        }
+
+        /// <summary>
+        /// Метод, возвращающий информацию о координатах робота.
+        /// </summary>
+        public string Position()
+        {
+            return String.Format("Позиция робота: x = {0}, y = {1}", x, y);
+        }
+
+    }
+
+    public class Program
+    {
+        delegate void Steps();
+
+        /// <summary>
+        /// Валидатор для ввода ограничений на перемещение робота.
+        /// </summary>
+        /// <param name="inp">
+        /// Входная строка.
+        /// </param>
+        /// <param name="maxX">
+        /// Максимальное значение x.
+        /// </param>
+        /// <param name="maxY">
+        /// Максимальное значение y.
+        /// </param>
+        /// <returns>
+        /// Если ввод корректен, возвращает true.
+        /// Иначе - false.
+        /// </returns>
+        /// <remarks>
+        /// Если пользователь решил отменить ввод, maxX и maxY приравниваются к -2.
+        /// </remarks>
+        static bool ValidateInp(string inp, out int maxX, out int maxY)
+        {
+            inp = inp.Trim(new char[2] { ' ', '\t' });
+
+            if (inp == "-")
+            {
+                maxX = -2;
+                maxY = -2;
+                return true;
+            }
+            else
+            {
+                // Сплиттим.
+                string[] inpSplitted = inp.Split(' ');
+
+                maxX = -1;
+                maxY = -1;
+
+                // Проверяем корректность введённых данных.
+                if (inpSplitted.Length != 2)
+                    return false;
+                else if (!int.TryParse(inpSplitted[0], out maxX) || maxX <= 0 || !int.TryParse(inpSplitted[1], out maxY)  || maxY <= 0)
+                    return false;  
+                else
+                    return true;
+            }
+        }
+
+        /// <summary>
+        /// Метод, обрабатывающий команду роботу.
+        /// </summary>
+        /// <param name="command">
+        /// Строка, содержащая команду.
+        /// </param>
+        /// <param name="area"> Поле, на котором перемещается робот.</param>
+        /// <param name="rob"> Сам робот. </param>
+        static void HandleComand (string comand, char[,] area, Robot rob)
+        {
+            Steps way = rob.Stay;
+
+            // Просматриваем все символы введённой команды.
+            for (int i = 0; i < comand.Length; ++i)
+                switch (comand[i])
+                {
+                    case 'R':
+                        way += rob.Right;
+                        break;
+
+                    case 'L':
+                        way += rob.Left;
+                        break;
+
+                    case 'F':
+                        way += rob.Forward;
+                        break;
+
+                    case 'B':
+                        way += rob.Backward;
+                        break;
+
+                    case ' ':
+                    case '\t':
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка: во введённой команде присутсвует какой-то символ кроме R, L, F и B." +
+                            "\n Попробуйте ещё раз.");
+                        return;
+                }
+
+            way();
+
+            // Проверяем, вышел ли робот за границы поля.
+            if (rob.leftArea)
+                Console.WriteLine("Робот чуть было не вышел за границы поля. Ход комманды был прерван.");
+
+            if (rob.x < 0)
+                rob.x++;
+            else if (rob.x >= rob.maxX)
+                rob.x--;
+            else if (rob.y < 0)
+                rob.y++;
+            else if (rob.y >= rob.maxY)
+                rob.y--;
+
+            Console.WriteLine(rob.Position());
+            rob.leftArea = false;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Добро пожаловать в программу управления роботом-снежинкой 69000!");
+            // Создаём робота.
+            Robot rob = new Robot();
+            rob.x = 0;
+            rob.y = 0;
+
+            Console.WriteLine("Введите максимальные допустимые x и y робота через пробел или -, чтобы завершить работу.");
+            while (!ValidateInp(Console.ReadLine(), out rob.maxX, out rob.maxY))
+                Console.WriteLine("Введены некорректные данные, попробуйте ещё раз или введите -, чтобы завершить работу.");
+
+            // Если пользователь решил отменить ввод.
+            if (rob.maxX == -2)
+                return;
+
+            Console.Clear();
+
+            // Поле.
+            char[,] area = new char[rob.maxY + 2, rob.maxX + 2];
+            rob.area = area;
+
+            for (int i = 0; i < rob.maxY + 2; ++i)
+            {
+                for (int j = 0; j < rob.maxX + 2; ++j)
+                    if (i == 0 || j == 0 || i == rob.maxY + 1 || j == rob.maxX + 1)
+                        area[i, j] = '6';
+                    else if (i == rob.y + 1 && j == rob.x + 1)
+                        area[i, j] = '*';
+                    else
+                        area[i, j] = ' ';
+            }
+
+            do
+            {
+                // Выводим подсказки.
+                Console.WriteLine("Робот-снежинка активирован.");
+                Console.WriteLine("Для управления им введите в строку произвольный набор символов R, L, F, B.");
+                Console.WriteLine("Каждая: ");
+                Console.WriteLine(" - Команда R перемещает робота вправо на одну клетку");
+                Console.WriteLine(" - Команда L перемещает робота влево на одну клетку");
+                Console.WriteLine(" - Команда F перемещает робота вперёд на одну клетку");
+                Console.WriteLine(" - Команда B перемещает робот назад на одну клетку");
+                Console.WriteLine($"Поле {rob.maxX}x{rob.maxY}: (здесь цифрами 6 отображаются границы поля)");
+
+                // Выводим поле.
+                for (int i = 0; i < rob.maxY + 2; ++i)
+                {
+                    for (int j = 0; j < rob.maxX + 2; ++j)
+                    {
+                        if (area[i, j] == '*')
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else if (area[i, j] == '+')
+                            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+
+                        Console.Write(area[i, j]);
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                        
+
+                    Console.WriteLine();
+                }
+
+                // Получаем от пользователя команду и выполняем её, если она корректна.
+                Console.WriteLine("Введите команду, которую должен будет исполнить робот или '-', чтобы завершить работу.");
+                string comand = Console.ReadLine().Trim(new char[2] {' ', '\t'});
+
+                // Если пользователь захотел завершить работу программы.
+                if (comand == "-")
+                    return;
+                else
+                {
+                    HandleComand(comand, area, rob);
+                    Console.WriteLine("Нажмите Enter, чтобы продолжить.");
+                    Console.ReadLine();
+
+                    // Чистим консоль.
+                    Console.Clear();
+                }
+            }
+            while (true);
+        }
+    }
+}
