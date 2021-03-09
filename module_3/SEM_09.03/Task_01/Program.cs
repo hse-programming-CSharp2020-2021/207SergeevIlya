1e34e2a95248435ee8b3f25be62d29234626a1a2
Iluhander <iisergeev_2@edu.hse.ru>
2021-03-09 17:46:04 +0300

    .

---
+++ module_3/SEM_09.03/Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Task_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fs = new FileStream(Console.ReadLine(), FileMode.Open))
+            {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, bytes.Length);
+                string txt = System.Text.Encoding.Default.GetString(bytes);
+
+                foreach (var symb in txt)
+                    if (symb >= '0' && symb <= '9')
+                        Console.WriteLine(symb);
+            }
+        }
+    }
+}
---
+++ module_3/SEM_09.03/Task_03/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int[] nums = new int[10];
+            for (int i = 0; i < nums.Length; ++i)
+                nums[i] = rand.Next();
+
+            string nums_parsed = "";
+            for (int i = 0; i < nums.Length; ++i)
+                nums_parsed += nums[i].ToString() + "\n";
+
+
+            // 1.
+            File.WriteAllText(@"../../../../MyTest1.txt", nums_parsed);
+            Console.WriteLine("В файл MyTest1.txt записано:\n" + File.ReadAllText(@"../../../../MyTest1.txt"));
+
+            // 2.
+            using (FileStream fs = new FileStream(@"../../../../MyTest2.txt", FileMode.OpenOrCreate))
+            {
+                byte[] bytes = System.Text.Encoding.Default.GetBytes(nums_parsed);
+
+                fs.Write(bytes);
+            }
+
+            Console.WriteLine("В файл MyTest2.txt записано:\n" + File.ReadAllText(@"../../../../MyTest2.txt"));
+
+            // 3.
+            using (StreamWriter sw = new StreamWriter(@"../../../../MyTest3.txt"))
+            {
+                sw.Write(nums_parsed);
+            }
+
+            Console.WriteLine("В файл MyTest3.txt записано:\n" + File.ReadAllText(@"../../../../MyTest3.txt"));
+
+            // 4.
+            using (BinaryWriter bw = new BinaryWriter(File.Open(@"../../../../MyTest4.txt", FileMode.OpenOrCreate)))
+            {
+                bw.Write(nums_parsed);
+            }
+
+            Console.WriteLine("В файл MyTest4.txt записано:\n" + File.ReadAllText(@"../../../../MyTest4.txt"));
+
+            // Считаем сумму чётных чисел.
+            int sum = 0;
+
+            using (StreamReader sr = new StreamReader(@"../../../../MyTest2.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+
+                    int num = int.Parse(line);
+                    if (num % 2 == 0)
+                        sum += num;
+                }
+            }
+
+            Console.WriteLine("Сумма чётных в файле MyTest2.txt:" + sum);
+        }
+    }
+}
